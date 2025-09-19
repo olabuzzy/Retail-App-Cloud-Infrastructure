79fda4f7d73ce544af5ac37ffd5e52bcdee0d5c4
Mijanscripts <mijansamuel@gmail.com>
2025-09-19 18:36:50 +0100

    ALB installation

---
+++ infra/alb.tf
@@ -0,0 +1,53 @@
+resource "aws_iam_policy" "alb_controller" {
+  name        = "AWSLoadBalancerControllerIAMPolicy"
+  description = "IAM policy for AWS Load Balancer Controller"
+  policy      = file("iam-policy.json") # same JSON from the AWS docs
+}
+
+resource "aws_iam_role" "alb_controller_role" {
+  name = "AmazonEKSLoadBalancerControllerRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "alb_controller_attach" {
+  role       = aws_iam_role.alb_controller_role.name
+  policy_arn = aws_iam_policy.alb_controller.arn
+}
+
+resource "kubernetes_service_account" "alb_controller_sa" {
+  metadata {
+    name      = "aws-load-balancer-controller"
+    namespace = "kube-system"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = aws_iam_role.alb_controller_role.arn
+    }
+  }
+}
+
+resource "helm_release" "aws_lb_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+
+  values = [
+    yamlencode({
+      clusterName    = aws_eks_cluster.name
+      serviceAccount = { create = false, name = kubernetes_service_account.alb_controller_sa.metadata[0].name }
+      region         = local.region
+      vpcId          = aws_vpc.altsch_vpc.id
+    })
+  ]
+  depends_on = [kubernetes_service_account.alb_controller_sa]
+}
+
