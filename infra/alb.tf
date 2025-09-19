af550e871aa5f31227d4606927afe6e9569acd95
Mijanscripts <mijansamuel@gmail.com>
2025-09-19 20:43:59 +0100

    new roles for alb-ingress to work properly with alb controller

--- infra/alb.tf
+++ infra/alb.tf
@@ -51,3 +51,66 @@ resource "helm_release" "aws_lb_controller" {
   depends_on = [kubernetes_service_account.alb_controller_sa]
 }
 
+
+
+# --- IAM Role for ALB Controller ---
+resource "aws_iam_role" "alb_controller" {
+  name = "alb-controller-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.eks.arn
+      },
+      Action = "sts:AssumeRoleWithWebIdentity",
+      Condition = {
+        StringEquals = {
+          "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub" = "system:serviceaccount:retail-dev:aws-load-balancer-controller"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "alb_controller_attach" {
+  role       = aws_iam_role.alb_controller.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSLoadBalancerControllerIAMPolicy"
+}
+
+# --- Kubernetes Service Account ---
+resource "kubernetes_service_account" "alb_controller_sa" {
+  metadata {
+    name      = "aws-load-balancer-controller"
+    namespace = "retail-dev"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = aws_iam_role.alb_controller.arn
+    }
+  }
+}
+
+# --- Deploy AWS Load Balancer Controller via Helm ---
+resource "helm_release" "aws_lb_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "retail-store"
+  version    = "1.7.2"
+
+  set {
+    name  = "clusterName"
+    value = aws_eks_cluster.eks.name
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = kubernetes_service_account.alb_controller_sa.metadata[0].name
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+}
+
